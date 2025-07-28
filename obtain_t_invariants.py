59169d7b0e908a11c0b0b3e55f43bd3ced745ea4
Cristian Tobar <cristiantobarov@gmail.com>
2025-07-28 15:00:36 -0600

    Create obtain_t_invariants function
    Function validated with exercises from Dr. Mariela

---
+++ obtain_t_invariants.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 28 12:16:35 2025
+
+@author: cristiantobar
+"""
+
+
+import os
+import re
+import pandas as pd
+from graphviz import Digraph
+import numpy as np
+import sympy as sp
+from pulp import LpProblem, LpMinimize, LpVariable, lpSum, LpInteger, LpStatus, value
+
+
+
+def calcular_componentes_test(C, transitions, places):
+    C_sym = sp.Matrix(C)
+    # -------------------------
+    # T-INVARIANTES
+    # -------------------------
+    nullspace_T = C_sym.nullspace()
+    t_invariantes_enteros = []
+    t_componentes_nombres = []
+
+    for vec in nullspace_T:
+        # Convertir a coeficientes enteros
+        lcm = sp.lcm([term.q for term in vec])
+        vec_int = [int(lcm * term) for term in vec]
+
+        # Incluir solo si todos los valores son ≥ 0 y alguno > 0
+        if all(v >= 0 for v in vec_int) and any(v > 0 for v in vec_int):
+            t_invariantes_enteros.append(vec_int)
+
+            if transitions:
+                indices = [i for i, val in enumerate(vec_int) if val != 0]
+                t_componentes_nombres.append([transitions[i] for i in indices])
+    
+    
+    # Matriz binaria (1 si transición participa, 0 si no)
+    t_invariantes_binarios = []
+    for vec in t_invariantes_enteros:
+        bin_vec = [1 if v != 0 else 0 for v in vec]
+        t_invariantes_binarios.append(bin_vec)
+
+    # -------------------------
+    # P-INVARIANTES
+    # -------------------------
+    C_sym_T = C_sym.T
+    nullspace_P = C_sym_T.nullspace()
+    p_componentes_nombres = []
+
+    for vec in nullspace_P:
+        lcm = sp.lcm([term.q for term in vec])
+        vec_int = [int(lcm * term) for term in vec]
+
+        if all(v >= 0 for v in vec_int) and any(v > 0 for v in vec_int):
+            if places:
+                indices = [i for i, val in enumerate(vec_int) if val != 0]
+                p_componentes_nombres.append([places[i] for i in indices])
+
+    return {
+        "matriz_C": C,
+        "t_invariantes_enteros": t_invariantes_enteros,
+        "t_invariantes_binarios": t_invariantes_binarios,
+        "t_componentes_nombres": t_componentes_nombres,
+        "p_componentes_nombres": p_componentes_nombres
+    }
+
+
+C_test = [
+    [-1,  0,  0,  0,  0,  0,  0,  0,  0, -1],
+    [ 1, -1,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  1, -1, -1, -1, -1, -1,  0,  0,  0],
+    [ 0,  0,  1,  0,  0,  0,  0, -1,  0,  0],
+    [ 0,  0,  0,  1,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  1,  0,  0,  0, -1,  0],
+    [ 0,  0,  0,  0,  0,  1,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  1,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  1,  1,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  1]
+]
+
+C_test_2 = [[-1, 1, 0, 0],
+            [-1, 1, -1, 1],
+            [0, 0, -1, 1],
+            [1, -1, 0, 0],
+            [0, 0, 1, -1],
+        ]
+
+
+#resultado2 = t_invariante_por_ILP(C_test_2)
+
+matriz_texto = """ 
+-1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+1	-1	-1	-1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	1	-1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	1	-1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	1	-1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	1	-1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	1	-1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	1	-1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	1	-1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	1	-1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	1	-1	-1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	-1	0	-1	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	1	-1	-1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	-1	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	-1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	-1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	-1	-1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	-1	-1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	-1	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	-1	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	1	-1	0	0	0	0	0	-1	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	-1	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	-1	0	0	1	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	-1	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	-1	0	-1	-1	-1	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	0	0	0	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	0	-1	0
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	0	-1
+0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	1	1
+"""
+# Convertir a lista de listas
+C_test_3 = [
+    list(map(int, line.strip().split()))
+    for line in matriz_texto.strip().split('\n')
+]
+
+transitions = [f"t{i+1}" for i in range(50)]
+places = [f"p{i+1}" for i in range(50)]
+
+resultado = calcular_componentes_test(C_test_3, transitions, places)
